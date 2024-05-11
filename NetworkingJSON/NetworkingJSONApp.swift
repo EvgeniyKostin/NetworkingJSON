1581ab1411c6313498e576195e9fdf0603ff4b44
Evgeniy Kostin <xqvynfs6f8@privaterelay.appleid.com>
2024-05-11 18:01:40 +0300

    Initial Commit

---
+++ NetworkingJSON/NetworkingJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingJSONApp.swift
+//  NetworkingJSON
+//
+//  Created by Evgeniy Kostin on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
