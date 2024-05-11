851122181e357bf120221a7106ca56eb5213a2ec
Evgeniy Kostin <xqvynfs6f8@privaterelay.appleid.com>
2024-05-11 21:41:48 +0300

    Task finished - data received.

--- NetworkingJSON/ContentView.swift
+++ NetworkingJSON/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var githubUserModel: GitHubUserModel?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            AsyncImage(url: URL(string: githubUserModel?.avatarUrl ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            } placeholder: {
+                Circle()
+                    .foregroundColor(.secondary)
+            }
+            .frame(width: 120)
+            
+            Text(githubUserModel?.login ?? "Login placeholder")
+                .bold()
+                .font(.title3)
+            
+            Divider()
+            
+            Text(githubUserModel?.bio ?? "Bio placeholder")
+                .padding()
+            
+            Spacer()
         }
         .padding()
+        .task {
+            do {
+                githubUserModel = try await getUser()
+            } catch GitHubError.invalidURL {
+                print("invalid URL")
+            } catch GitHubError.invalidResponse {
+                print("invalid response")
+            } catch GitHubError.invaliData {
+                print("invalid data")
+            } catch {
+                print("unexpected error")
+            }
+        }
     }
 }
 
--- NetworkingJSON/GitHubUserModel.swift
+++ NetworkingJSON/GitHubUserModel.swift
@@ -12,3 +12,39 @@ struct GitHubUserModel: Codable {
     let avatarUrl: String
     let bio: String
 }
+//class GitHubUserModel: Codable {
+//    var login: String = ""
+//    var avatarUrl: String = ""
+//    var bio: String = ""
+//
+//    init(login: String, avatarUrl: String, bio: String) {
+//        self.login = login
+//        self.avatarUrl = avatarUrl
+//        self.bio = bio
+//    }
+//}
+
+func getUser() async throws -> GitHubUserModel {
+    let endPoint = "https://api.github.com/users/EvgeniyKostin"
+    guard let url = URL(string: endPoint) else {
+        throw GitHubError.invalidURL }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    guard let response = response  as? HTTPURLResponse, response.statusCode == 200 else {
+        throw GitHubError.invalidResponse
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try decoder.decode(GitHubUserModel.self, from: data)
+    } catch {
+        throw GitHubError.invaliData
+    }
+}
+
+enum GitHubError: Error {
+    case invalidURL
+    case invalidResponse
+    case invaliData
+}
